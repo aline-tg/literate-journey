b64c0a66c8c5753fb984f80164ab9b15665a871e
Aline T <43286541+aline-tg@users.noreply.github.com>
2022-01-28 21:36:05 -0300

    first start

---
+++ literate-journey/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/Header';
+import { MoviesList } from './components/MoviesList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <section>
+        <MoviesList movies = {[]} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
